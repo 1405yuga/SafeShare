9da70a16056c13c4ac747832a68cb705619449d2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-06-04 15:24:28 +0530

    created preview annotations

---
+++ app/src/main/java/com/example/safeshare/utils/annotations/VerticalScreenPreview.kt
@@ -0,0 +1,10 @@
+package com.example.safeshare.utils.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 640
+)
+annotation class VerticalScreenPreview
