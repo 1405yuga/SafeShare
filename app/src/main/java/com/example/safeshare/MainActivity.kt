e281ed870439178f3739482d9156e64f71dd2705
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-06-05 02:37:39 +0530

    removed extra padding

--- app/src/main/java/com/example/safeshare/MainActivity.kt
+++ app/src/main/java/com/example/safeshare/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,8 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SafeShareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SafeSecureApp(modifier = Modifier.padding(innerPadding))
+                Surface {
+                    SafeSecureApp()
                 }
             }
         }
